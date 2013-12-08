63df251c3287e1388aa85af99e7cb60afaf367ca
Antonio Casero Palmero <anto.casero@gmail.com>
2013-12-08 18:56:10 +0100

    Sample project updated.

--- ACPScrollMenu/ViewController.m
+++ ACPScrollMenu/ViewController.m
@@ -16,29 +16,37 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-    
 	[self setUpACPScroll];
-    
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
 }
 
 - (void)setUpACPScroll {
 	NSMutableArray *array = [[NSMutableArray alloc] init];
-	for (int i = 1; i < 5; i++) {
+	for (int i = 1; i < 5; i++)
+    {
 		NSString *imgName = [NSString stringWithFormat:@"%d.png", i];
 		NSString *imgSelectedName = [NSString stringWithFormat:@"%ds.png", i];
         
-		//Set the items
-		ACPItem *item = [[ACPItem alloc] initACPItem:[UIImage imageNamed:@"bg.png"] iconImage:[UIImage imageNamed:imgName] andLabel:@"Test"];
+		//You can choose between work with delegates or with blocks! up to you!
+		//This sample project has commented the delegate functionality
+        
+		//ACPItem *item = [[ACPItem alloc] initACPItem:[UIImage imageNamed:@"bg.png"] iconImage:[UIImage imageNamed:imgName] andLabel:@"Test"];
+        
+		//Item working with blocks
+		ACPItem *item = [[ACPItem alloc] initACPItem:[UIImage imageNamed:@"bg.png"]
+                                           iconImage:[UIImage imageNamed:imgName]
+                                               label:[NSString stringWithFormat:@"Test%d", i]
+                                           andAction: ^(ACPItem *item) {
+                                               
+                                               NSLog(@"Block called! %d", i);
+                                               //DO somenthing here
+                                           }];
         
 		//Set highlighted behaviour
 		[item setHighlightedBackground:nil iconHighlighted:[UIImage imageNamed:imgSelectedName] textColorHighlighted:[UIColor redColor]];
-        
 		[array addObject:item];
 	}
     
@@ -46,13 +54,12 @@
     
 	//We choose an animation when the user touch the item (you can create your own animation)
 	[_ACPScrollMenu setAnimationType:ACPZoomOut];
-    
 	_ACPScrollMenu.delegate = self;
 }
 
-- (void)scrollMenu:(ACPItem *)menu didSelectIndex:(NSInteger)selectedIndex {
-	NSLog(@"Item %d", selectedIndex);
-    //DO somenthing here
-}
+//- (void)scrollMenu:(ACPItem *)menu didSelectIndex:(NSInteger)selectedIndex {
+//	NSLog(@"Item %d", selectedIndex);
+//    //DO somenthing here
+//}
 
 @end
